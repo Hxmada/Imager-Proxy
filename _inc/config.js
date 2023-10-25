26d8224a68d046629533769adcbea9f570b0eb16
Hxmada <56478508+Hxmada@users.noreply.github.com>
2023-10-26 02:58:53 +0300

    Update config.js

--- _inc/config.js
+++ _inc/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* ---------------------- Main Config ---------------------- */
   imagerURL: 'https://imager.zipto.net/?figure=', // Actual Imager URL
-  userIDURL: '/userid/:userID',
+  userIDURL: '/userid/:userID', // UserID URL
   usernameURL: '/username/:username', // Username URL
   figureURL: '/figure/:look', // Figure URL
   port: 3930, // Port to run your Imager proxy
